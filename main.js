feaf25bbb7e027c2a28d431fbe626921f670e07d
delantsheerian <45123632+delantsheerian@users.noreply.github.com>
2018-11-17 16:37:45 +0100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-//var maakKeuze = window.parseFloat(prompt("Wilt u een groot pak, een klein of een mini?"));
-//var saus = window.parseFloat(prompt("Wilt u er saus bij?"));
-//var drank = window.parseFloat(prompt("Wilt u er drank bij?"));
-//var totaal = frieten + saus + drank;
-//
-//document.getElementById("totaal").innerHTML = ("Het te betalen bedragen bedraagt : " + totaal);
-//
